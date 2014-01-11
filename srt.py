70c99d195c6cb992c130626a87eb9a53dfc25865
shinigamiryuk <sharibuntu@gmail.com>
2014-01-11 15:46:55 -0500

    SRT files download improved

--- srt.py
+++ srt.py
@@ -1,55 +1,39 @@
-'''
-    The link that I was trying to download was this
-    https://class.coursera.org/compilers-003/lecture/subtitles?q=1_en&format=srt
-    
-    If you want to use this script to download srt's then just change the url in the code accordingly.
-    
-'''
-
+import sqlite3 as lite
 import os
-import glob
-from urllib import unquote
+import sys
+from module_2 import download, scrape
 
-def unquote_u(source):
-    res = unquote(source)
-    if '%u' in result:
-        res = result.replace('%u','\\u').decode('unicode_escape')
-    return res
+page_url = 'https://class.coursera.org/compilers-003/lecture'
 
+'''
+Handling Database 
+'''
 
-os.system("mkdir CourseEra")
+con = None
 
-# the initial and final values of documents to be downloaded
-initial_value = raw_input("Enter the initial number: ")
-final_value = raw_input("Enter the final number: ")
+if not(os.path.exists('database.db')):
+	con = lite.connect('database.db')	
+	urls = scrape(page_url)
 
-init = int(initial_value)
-final = int(final_value)
+	with con:
+		cur = con.cursor()
+		cur.execute("CREATE TABLE links(url TEXT)")
 
-for ctr in range(init, final + 1):
+		for link in urls:
+			cur.execute("INSERT INTO links VALUES(?)", (link,))			
 
-	# Download the file
-	url = 'wget --content-disposition --no-http-keep-alive --quiet "https://class.coursera.org/compilers-003/lecture/subtitles?q=' + str(ctr) + '_en&format=srt"'
-	os.system(url)
+else:
+	con = lite.connect('database.db')
 
-	# Rename the file
-	result = []
-	result = glob.glob('*.srt')
+	with con:
+		cur = con.cursor()
+		cur.execute("SELECT * FROM links")
 
-	for i in range(0, len(result)):
-		x = result[i]
-		encoded_name = x[:len(x)/2]
-		actual_name = unquote_u(encoded_name)
-		
-		# Rename the file into its actual name
-		os.rename(x, actual_name)
-		
-		# Move the files into CourseEra Directory
-		move_cmd = "find . -name \*.srt -exec cp {} CourseEra/ \;"
-		os.system(move_cmd)
+		rows = cur.fetchall()
 
-		# Remove all .srt files from parent directory
-		del_cmd = "rm *.srt"
-		os.system(del_cmd)
-
-os.popen('notify-send "CourseEra Notification" "All subtitles successfully downloaded :)"' )
+		for row in rows:
+			print 'Downloading, '
+			download(row[0])
+			cur.execute("DELETE FROM links WHERE url=?", (row[0],))
+			con.commit()
+			print 'Deleted: ', row[0]
